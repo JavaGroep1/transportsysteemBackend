a901fcae0a5f37e607c57b939714b4986e0eb236
joep van diessen <joepvandiessen@gmail.com>
2020-10-30 13:29:23 +0100

    vehicle controller update

--- BackofficeBackend/src/main/java/com/joep/backofficeapi/Controllers/VehicleController.java
+++ BackofficeBackend/src/main/java/com/joep/backofficeapi/Controllers/VehicleController.java
@@ -24,36 +24,38 @@ public class VehicleController {
     private VehicleContainer vehicleContainer;
 
     @PostMapping("/add")
-    public ResponseEntity<?> addVehicle(@RequestBody AddVehicleRequest vehicle){
+    public ResponseEntity<?> addVehicle(@RequestBody AddVehicleRequest vehicle) {
         vehicleContainer.addVehicle(new Vehicle(
                 vehicle.getLicensePlate(),
                 vehicle.getVehicleCategory(),
                 vehicle.getCapacityInKg()
-                ));
+        ));
         return ResponseEntity.ok("ok");
     }
 
     @GetMapping("")
-    public ResponseEntity<?> getVehicles(String licensePlate, VehicleCategory category) throws VehicleNotFoundException {
-        if (category != null){
-            return ResponseEntity.ok(vehicleContainer.getVehiclesByCategory(category));
-        }
-        if (licensePlate != null){
-            return ResponseEntity.ok(vehicleContainer.getVehicleByPlate(licensePlate));
-        }
+    public ResponseEntity<?> getVehicles() throws VehicleNotFoundException {
         return ResponseEntity.ok(vehicleContainer.getVehicles());
+    }
+
+    @GetMapping(value = "", params = "licensePlate")
+    public ResponseEntity<?> getVehicles(String licensePlate) throws VehicleNotFoundException {
+        return ResponseEntity.ok(vehicleContainer.getVehicleByPlate(licensePlate));
+    }
+
+    @GetMapping(value = "", params = "category")
+    public ResponseEntity<?> getVehicles(VehicleCategory category) throws VehicleNotFoundException {
+        return ResponseEntity.ok(vehicleContainer.getVehiclesByCategory(category));
     }
 
     @PutMapping("")
     public ResponseEntity<?> editVehicle(@RequestBody EditVehicleRequest request) throws VehicleNotFoundException {
-
         vehicleContainer.updateVehicle(request);
         return ResponseEntity.ok("Updated");
-
     }
 
     @DeleteMapping("")
-    public ResponseEntity<?> removeVehicle(String vehicleid){
+    public ResponseEntity<?> removeVehicle(String vehicleid) {
         vehicleContainer.deleteVehicle(new ObjectId(vehicleid));
         return ResponseEntity.ok("deleted");
     }
